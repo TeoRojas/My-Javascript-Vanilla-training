34712c608ace2f21686f7ace2b8f130eb7d4a6de
TeoRojas <teo@iMac-de-Teofilo.local>
2020-08-10 22:35:07 +0200

    newMorphism OK

--- TODO List exercise/app.js
+++ TODO List exercise/app.js
@@ -1,12 +1,24 @@
 let texto = document.getElementById('input-text').value;
-const button = document.getElementById("ok-button");
+let button = document.getElementById("add-btn");
 
 
-button.addEventListener("click", function(e){
+button.addEventListener('click', function(e){
     e.preventDefault();
     texto = document.getElementById('input-text').value;
     console.log(texto);
 });
 
+console.log(button.children[0].classList);
 
+button.addEventListener('onmouseover', function(e){
+    e.preventDefault();
+    button.children[0].classList.add('icon-blue');
+    console.log('estoy encima del botón');
+});
 
+button.addEventListener('onmouseout', function(e){
+    e.stopPropagation;
+    button.children[0].classList.remove('icon-blue');
+    console.log('estoy fuera del botón');
+
+})
